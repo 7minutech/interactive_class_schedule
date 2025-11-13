4af46055a0fafea2d9aa5f2de3c4e588f27d9137
7minutech <elijohnson1107@gmail.com>
2025-11-13 13:10:54 -0600

    refactor: should use subject id not name

--- routes/subject.js
+++ routes/subject.js
@@ -4,27 +4,19 @@ import { db } from "../store/db.js"
 import { fetchSubjectID } from '../utils/subjectHelper.js';
 
 router.get('/', (req, resp) => {
-    const { subjectName } = req.query;
+    const { subjectID } = req.query;
     
-    if (!subjectName) {
-         db.all(`SELECT * FROM subject`, [], (err, rows) => {
-            if (err) {
-                resp.status(500).json({error: "database had error finding subject"})
-            }   
-            if (!rows){
-                resp.status(404).json({error: "subject not found"})
-            }
+    if (!subjectID) {
+        db.all(`SELECT * FROM subject`, [], (err, rows) => {
+            if (err) return resp.status(500).json({error: "database had error finding subjects"})
+            if (!rows) return resp.status(404).json({error: "subjects not found"})
             resp.status(200).json(rows)
         });
     }
     else {
-            db.get(`SELECT * FROM subject where name = ?`, [subjectName], (err, row) => {
-            if (err) {
-                resp.status(500).json({error: "database had error finding subject"})
-            }   
-            if (!row){
-                resp.status(404).json({error: "subject not found"})
-            }
+        db.get(`SELECT * FROM subject where id = ?`, [subjectID], (err, row) => {
+            if (err) return resp.status(500).json({error: "database had error finding subject"})
+            if (!row) return resp.status(404).json({error: "subject not found"})
             resp.status(200).json(row)
         });
     }
