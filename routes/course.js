ea09454f652d35c5060f66c45111fe224bc74efa
7minutech <elijohnson1107@gmail.com>
2025-11-13 12:41:34 -0600

    refactor: get courses route

--- routes/course.js
+++ routes/course.js
@@ -6,15 +6,30 @@ import { fetchSubjectID } from '../utils/subjectHelper.js';
 router.get('/', (req, resp) => {
     const { courseNum } = req.query; 
 
-    db.all(`SELECT * FROM course where num = ?`, [courseNum], (err, rows) => {
-        if (err) {
-            resp.status(500).json({error: "database had error finding courses"})
-        }   
-        if (!rows){
-           resp.status(404).json({error: "courses not found"})
-        }
-        resp.status(200).json(rows)
-    });
+    if (!courseNum) {
+        db.all(`SELECT * FROM course`, [], (err, rows) => {
+            if (err) {
+                resp.status(500).json({error: "database had error finding courses"})
+            }   
+            if (!rows){
+            resp.status(404).json({error: "courses not found"})
+            }
+            resp.status(200).json(rows)
+        });
+    }
+    else {
+        db.all(`SELECT * FROM course where num = ?`, [courseNum], (err, rows) => {
+            if (err) {
+                resp.status(500).json({error: "database had error finding courses"})
+            }   
+            if (!rows){
+            resp.status(404).json({error: "courses not found"})
+            }
+            resp.status(200).json(rows)
+        });
+    }
+
+   
 });
 
 export default router;
