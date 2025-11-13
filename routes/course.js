0ded4f209250c83d638a9a8f0b7d48a1c6a966e4
7minutech <elijohnson1107@gmail.com>
2025-11-13 14:05:32 -0600

    refactor: added courseNum & subject id

--- routes/course.js
+++ routes/course.js
@@ -4,13 +4,20 @@ import { db } from "../store/db.js"
 import { fetchSubjectID } from '../utils/subjectHelper.js';
 
 router.get('/', (req, resp) => {
-    const { courseNum } = req.query; 
+    const { courseNum, subjectID } = req.query; 
 
     if (!courseNum) {
         db.all(`SELECT * FROM course`, [], (err, rows) => {
             if (err) return resp.status(500).json({error: "database had error finding courses"})  
             if (!rows) return resp.status(404).json({error: "courses not found"})
             resp.status(200).json(rows)
+        });
+    }
+    else if (courseNum && subjectID) {
+         db.get(`SELECT * FROM course where num = ? AND subjectid = ?`, [courseNum, subjectID], (err, row) => {
+            if (err) return resp.status(500).json({error: "database had error finding courses"})  
+            if (!row) return resp.status(404).json({error: "courses not found"})
+            resp.status(200).json(row)
         });
     }
     else {
