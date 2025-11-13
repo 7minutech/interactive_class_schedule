5fa20c5d21f0af9f7afa1dd6f17642b0334b2f57
7minutech <elijohnson1107@gmail.com>
2025-11-13 12:36:44 -0600

    refactor: get subjects route

--- routes/subject.js
+++ routes/subject.js
@@ -4,17 +4,32 @@ import { db } from "../store/db.js"
 import { fetchSubjectID } from '../utils/subjectHelper.js';
 
 router.get('/', (req, resp) => {
-    const { subjectName } = req.query; 
+    const { subjectName } = req.query;
+    
+    if (!subjectName) {
+         db.all(`SELECT * FROM subject`, [], (err, rows) => {
+            if (err) {
+                resp.status(500).json({error: "database had error finding subject"})
+            }   
+            if (!rows){
+                resp.status(404).json({error: "subject not found"})
+            }
+            resp.status(200).json(rows)
+        });
+    }
+    else {
+            db.get(`SELECT * FROM subject where name = ?`, [subjectName], (err, row) => {
+            if (err) {
+                resp.status(500).json({error: "database had error finding subject"})
+            }   
+            if (!row){
+                resp.status(404).json({error: "subject not found"})
+            }
+            resp.status(200).json(row)
+        });
+    }
 
-    db.get(`SELECT * FROM subject where name = ?`, [subjectName], (err, row) => {
-        if (err) {
-            resp.status(500).json({error: "database had error finding subject"})
-        }   
-        if (!row){
-           resp.status(404).json({error: "subject not found"})
-        }
-        resp.status(200).json(row)
-    });
+
 });
 
 export default router;
