2dfa2726ef907af2a0c826de834349d1c684a420
7minutech <elijohnson1107@gmail.com>
2025-11-19 21:08:12 -0600

    feat: landing page

---
+++ public/scripts/LandingPage.jsx
@@ -0,0 +1,71 @@
+class LandingPage extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            terms: [],
+            loading: true,
+            selectedTerm: '',
+        }
+    }
+
+    componentDidMount = () => {
+        this.fetchTerms()
+    }
+        
+    fetchTerms = () => {
+        fetch('/terms')
+        .then((res) => {
+            console.log('Response status:', res.status);
+            return res.json();
+        })
+        .then((data) => {
+            console.log('Fetched data:', data);
+            this.setState({ terms: data, loading: false });
+        })
+        .catch((err) => {
+            console.error('Fetch error:', err);
+            this.setState({ loading: false });
+        });
+    };
+
+    onChange = (event) => {
+        this.setState({ selectedTerm: event.target.value });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        const { selectedTerm } = this.state;
+        if (selectedTerm) {
+        window.location.href = `/search.html`; 
+        }
+    };
+
+    render() {
+        const { terms, loading, selectedTerm } = this.state;
+
+        if (loading) {
+        return (
+            <select disabled>
+            <option>Loading...</option>
+            </select>
+        );
+        }
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <select value={selectedTerm} onChange={this.onChange}>
+                <option value="" disabled>
+                    Select a term
+                </option>
+                {terms.map((term) => (
+                    <option key={term.id} value={term.id}>
+                    {term.name}
+                    </option>
+                ))}
+                </select>
+                <button type="submit">Go</button>
+            </form>
+        );
+    }
+}
