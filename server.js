3c9bbd76ec14322a302f7d6f3ff537e4ed90ead6
7minutech <elijohnson1107@gmail.com>
2025-11-20 20:14:15 -0600

    feat: /search send search.html

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ import scheduleTypeRouter from "./routes/scheduleType.js"
 import resultRouter from "./routes/result.js"
 import termRouter from "./routes/term.js"
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 app.use(cors());
@@ -15,6 +21,10 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.set('view engine', 'ejs');
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'search.html'));
+});
+
 
 app.use('/sections', sectionRouter);
 app.use('/subjects', subjectRouter);
