9c55ebf4f5f7e8068be75791be5820743d322ff4
7minutech <elijohnson1107@gmail.com>
2025-11-22 20:44:45 -0600

    feat: start time input w/ to24Hour helper

--- public/scripts/SearchPage.jsx
+++ public/scripts/SearchPage.jsx
@@ -1,3 +1,7 @@
+const HOURS_12 = Array.from({ length: 13 }, (_, i) => i.toString().padStart(2, "0"));
+const MINUTES = Array.from({ length: 4 }, (_, i) => (i * 15).toString().padStart(2, "0"));
+const AM_PM = ["AM", "PM"]; 
+
 class SearchPage extends React.Component {
 
     constructor(props) {
@@ -11,7 +15,10 @@ class SearchPage extends React.Component {
             selectedSubject: '',
             selectedScheduleType: '',
             selectedCourseLevel: '',
-            selectedCourseNumber: ''
+            selectedCourseNumber: '',
+            selectedStartTime: '00:00',
+            selectedStartTimePeriod: 'AM',
+            
         }
     }
 
@@ -20,6 +27,36 @@ class SearchPage extends React.Component {
         const term = params.get("term");
         this.setState({term: term})
         this.fetchAll()
+    }
+
+    to24Hour(hour, period) {
+        let parsedHour = parseInt(hour)
+
+        if (parsedHour > 12) {
+            parsedHour -= 12
+        }
+
+        if (period == "PM" && this.state.selectedStartTimePeriod == "AM" && hour == "00") {
+            return "12"
+        }
+
+        if (hour == "00") {
+            return hour
+        }
+
+        if (parsedHour === 12) {    
+            if (period == "AM") {
+                return "00"
+            }
+            return hour
+        } else {
+            if (period == "AM") {
+                return parsedHour.toString().padStart(2, "0")
+            }
+            return (parsedHour + 12).toString()
+
+        }
+        
     }
     
 
@@ -58,6 +95,25 @@ class SearchPage extends React.Component {
     onCourseNumberChange = (event) => {
         this.setState({ selectedCourseNumber: event.target.value });
     };
+
+    onStartTimeHourChange = (event) => {
+        let hour24 = this.to24Hour(event.target.value, this.state.selectedStartTimePeriod)
+        this.setState({ selectedStartTime:  hour24 +  ":" + this.state.selectedStartTime.substring(3) });
+        console.log(hour24 +  ":" + this.state.selectedStartTime.substring(3))
+    }
+
+    onStartTimeMinuteChange = (event) => {
+        this.setState({ selectedStartTime:  this.state.selectedStartTime.substring(0, 2) + ":" + event.target.value});
+        console.log(this.state.selectedStartTime.substring(0, 2) + ":" + event.target.value)
+    }
+    
+    onStartTimePeriodChange = (event) => {
+        let startTimeHour = this.state.selectedStartTime.substring(0,2)
+        let hour24 = this.to24Hour(startTimeHour, event.target.value)
+        this.setState({ selectedStartTime:  hour24 +  ":" + this.state.selectedStartTime.substring(3) });
+        this.setState({ selectedStartTimePeriod: event.target.value });
+        console.log(hour24 +  ":" + this.state.selectedStartTime.substring(3))
+    }
 
     render() {
 
@@ -112,6 +168,26 @@ class SearchPage extends React.Component {
                         ))}
                     </select>
 
+                </div>
+                <div className="input_container">
+                    <label htmlFor="start_time_hour">Start Time Hour:</label>
+                    <select name="start_time_hour" id="start_time_hour" onChange={this.onStartTimeHourChange}>
+                        {
+                            HOURS_12.map((hour) => (<option value={hour}>{hour}</option>))
+                        }
+                    </select>
+                    <label htmlFor="start_time_minute">Minute:</label>
+                    <select name="start_time_minute" id="start_time_minute" onChange={this.onStartTimeMinuteChange}>
+                        {
+                            MINUTES.map((minute) => (<option value={minute}>{minute}</option>))
+                        }
+                    </select>
+                    <label htmlFor="start_time_AM/PM">AM/PM:</label>
+                    <select name="start_time_AM/PM" id="start_time_AM/PM" onChange={this.onStartTimePeriodChange} >
+                        {
+                            AM_PM.map((period) => (<option value={period}>{period}</option>))
+                        }
+                    </select>
                 </div>
                 <div>
                     <button type="submit" >Search</button>
